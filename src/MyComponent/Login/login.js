ce4acf80b930e7747a5c9aa5610aa2ef0a57c4d7
Puneetphull <Puneetphull21@gmail.com>
2021-05-17 16:30:50 +0530

    login update

--- src/MyComponent/Login/login.js
+++ src/MyComponent/Login/login.js
@@ -5,21 +5,37 @@ import Button from '@material-ui/core/Button';
 //import Icon from '@material-ui/core/Icon';
 import './login.css';
 
+
 export default class login extends Component {
+
+    constructor(){
+        super();
+        this.state={
+            email:"",
+            password:"",
+            
+            
+        }
+    }
+     onsubmit(){
+         alert("check the console.log");
+         console.log(this.state);         
+     }
+    
     render() {
         return (
             <div className="Login">
              <h2 style={{textAlign:"center",color:"blue",fontFamily:"cursive"}}>Login Page</h2>
              <Card className="loginCard">
-             <h2 style={{color:"blue",marginLeft:"20px"}}>login With you account</h2>
-             <TextFiled style={{width:"80%",marginLeft:"2%",marginTop:"20px"}} required type="text"  id="outlined-basic" label="Email" variant="outlined"/>
+             <h2 style={{color:"blue",marginLeft:"20px"}}>login With your account</h2>
+             <form  >
+             <TextFiled style={{ width:"80%",marginLeft:"2%",marginTop:"20px"}} name="email" required type="text"  label="Email" variant="outlined" onChange={(f)=>{this.setState({email:f.target.value})}} />
+             <TextFiled style={{ width:"80%",marginLeft:"2%",marginTop:"20px"}} name="password" required type="Password"   label="Password" variant="outlined" onChange={(f)=>{this.setState({password:f.target.value})}} />
              <br></br>
              <br></br>
-             <TextFiled   style={{width:"80%",marginLeft:"2%"}} required type="Password" name="password" id="outlined-basic" label="Password" variant="outlined" />
+             <Button style={{marginBottom:"10px",marginLeft:"20px"}} onClick={()=>this.onsubmit()}  variant="contained" color="primary" >Login </Button>
              <br></br>
-             <br></br>
-             <Button style={{marginBottom:"10px",marginLeft:"20px"}} variant="contained" color="primary" >Login </Button>
-             <br></br>
+             </form>
              <p style={{color:"blue",marginLeft:"10px"}}>Don't have account signup</p>
              </Card> 
              </div>
